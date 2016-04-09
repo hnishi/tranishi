db8be44b60db579f3880fb10bcf6d2c74a51936e
hnishi <hnishigami@protein.osaka-u.ac.jp>
2016-04-09 14:57:15 +0900

    delete comments in main.cpp
    for simplicity

--- main.cpp
+++ main.cpp
@@ -2,28 +2,24 @@
 
 using namespace std;
 
-
 int main(int argc, char *argv[]){
    cout<<"Version info. tranishi v1.3.5 \n";
 // ##################### ARGUMENT HANDLING ##########################
-// argv[1]: input file
   if( argv[1]==NULL ){
     puts("No ARGUMEMTS");
-    //puts("USAGE: ./a.out (input parameter file) ");
     puts("USAGE: ./a.out (trajectory) (pdb file) (optional: FRAME INTERVAL) (optional: ATOM SELECTION)");
     puts("USAGE: selection: all, protein, heavy, mainchain, ca  ");
     return 1;
   }
-  //cout<<"Your input files: "<<argv[1]<<endl;
-
+  
 // INPUT_PARAMETERS
-//Inp_nishi inp1( argv[1] );
 
   int stride = 1;
   string selectatom = "all";
 
   string codname = argv[1];
   string pdbname = argv[2];
+
   string argu3; 
   if( argv[3] ){
     argu3 = argv[3];
@@ -33,6 +29,7 @@ int main(int argc, char *argv[]){
     selectatom = argu3;
     stride = 1;
   }}
+
   string argu4;
   if( argv[4] ){argu4= argv[4];
   if(atoi(argu4.c_str())){stride=atoi(argu4.c_str());}
@@ -40,25 +37,19 @@ int main(int argc, char *argv[]){
           ||argu4=="mainchain"||argu4=="ca"){
     selectatom = argu4;
   }}
-  //if( argv[4] )selectatom = argv[4];
 
 // Tranishi
 	cout<<"### your input information ###\n";
-    cout<<"ATOM SELECTION = "<<selectatom<<endl;
+　　    cout<<"ATOM SELECTION = "<<selectatom<<endl;
 	cout<<"FRAME INTERVAL = "<<stride<<endl;
-	//tra_nishi tra1("../samp/md_pra.cod","../samp/pra.pdb","protein");
-	//tra_nishi tra1("samp/md_npt_10.cod","samp/md0.out.pdb",1);
 	cout<<"### now loading cod file ###\n";
 	tra_nishi tra1( codname.c_str(), pdbname.c_str(),stride, selectatom.c_str() );
-	//cout<<"ORIGINAL TOTAL STEP = "<<tra1.total_step*stride<<endl;
 	cout<<"TOTAL FRAME = "<<tra1.total_step<<endl;
 	//tra1.pdb1->write_pdb("xxx.pdb");
 	//tra1.write_step("md.pdb", 0);
 	//tra1.write_cod("zzz.pdb",1);
 	//tra1.fix_cod_npt();
 	//tra1.fix_cod(77.63074,80.07944,158.1261);  // for md_pra.cod and pra.pdb
-    //
-    //
 	if(tra1.total_step/stride>500 && selectatom == "all"){
 		puts("WARNING: Number of frames of your input is more than 500.");
 		puts("You can set a third argument as a stride number when the output file is too large to show movies with some viewer.");
@@ -74,7 +65,6 @@ int main(int argc, char *argv[]){
 	}*/
 
 	cout<<"### now writing trajectory in ASCII ###\n";
-	//tra1.write_cod( "out_traj.pdb", stride);
 	tra1.write_cod( "out_traj.pdb", 1);
 	cout<<"output out_traj.pdb"<<endl;
 // END
